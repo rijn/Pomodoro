eb696cec6a987a0169684867b3018076de86b4f1
Rijn <bxbian951122@gmail.com>
2018-07-29 18:03:40 -0700

    Initial build

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '11.0'
+
+target 'Pomodoro' do
+  use_frameworks!
+
+  pod 'TinyConstraints'
+  pod 'ViewAnimator'
+  pod 'DynamicColor', '~> 4.0.1'
+  pod 'SwiftIconFont'
+
+end
---
+++ Pomodoro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Rijn on 7/29/18.
+//  Copyright © 2018 Rijn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
