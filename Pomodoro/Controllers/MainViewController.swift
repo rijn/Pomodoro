62f929362dec20301fb170544116b086293502c2
Rijn <bxbian951122@gmail.com>
2018-08-13 18:16:23 -0500

    Implement icloud and vibration

--- Pomodoro/Controllers/MainViewController.swift
+++ Pomodoro/Controllers/MainViewController.swift
@@ -9,8 +9,19 @@
 import Foundation
 import UIKit
 import TinyConstraints
+import AudioToolbox
 
 class MainViewController: UIViewController {
+    
+    var currentTime: TimeInterval = NSDate().timeIntervalSince1970
+    var targetTime: TimeInterval = 0
+    var offsetTime: TimeInterval = 0
+    var isPanning: Bool = false
+    var timer: Timer? = nil
+    let generator = UIImpactFeedbackGenerator(style: .light)
+    
+    var iCloudKeyStore: NSUbiquitousKeyValueStore? = NSUbiquitousKeyValueStore()
+    let iCloudSavedTargetTimeKey = "TargetTimestamp"
     
     var timerViewController: TimerViewController = {
         let timerViewController = TimerViewController()
@@ -32,11 +43,75 @@ class MainViewController: UIViewController {
         timerViewController.view.center(in: self.view)
         
         let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureHandler(_:)))
+        gestureRecognizer.maximumNumberOfTouches = 1
         view.addGestureRecognizer(gestureRecognizer)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        
+        iCloudSetUp()
     }
     
     @objc func panGestureHandler(_ recognizer: UIPanGestureRecognizer) {
-        timerViewController.setTime(second: 600)
-        print(recognizer)
+        let previousTargetInterval = targetTime + offsetTime - currentTime
+        offsetTime = -TimeInterval(recognizer.translation(in: self.view).x)
+        let targetInterval = targetTime + offsetTime - currentTime
+        if targetInterval >= Double((floor(previousTargetInterval / 60) + 1) * 60) || previousTargetInterval >= Double((floor(targetInterval / 60) + 1) * 60){
+            generator.impactOccurred()
+        }
+        
+        switch recognizer.state {
+        case .began:
+            isPanning = true
+            break
+        case .changed:
+            break
+        case.ended:
+            isPanning = false
+            targetTime = targetTime + offsetTime
+            offsetTime = 0
+            saveTimerToiCloud()
+            break
+        default:
+            break
+        }
+        
+        if targetInterval  < 0 {
+            targetTime = currentTime - offsetTime
+        }
+        if targetInterval > 3600 {
+            targetTime = 3600 + currentTime - offsetTime
+        }
+        updateTimerView()
+    }
+    
+    @objc func timerAction() {
+        currentTime = NSDate().timeIntervalSince1970
+        if targetTime < currentTime {
+            targetTime = currentTime
+        }
+        updateTimerView()
+    }
+    
+    func updateTimerView() {
+        timerViewController.setTime(second: Int(targetTime + offsetTime - currentTime));
+    }
+    
+    func saveTimerToiCloud() {
+        iCloudKeyStore?.set(Double(targetTime), forKey: iCloudSavedTargetTimeKey)
+        iCloudKeyStore?.synchronize()
+    }
+    
+    func iCloudSetUp() {
+        if let savedTargetTime = iCloudKeyStore?.double(forKey: iCloudSavedTargetTimeKey) {
+            targetTime = savedTargetTime
+        }
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(ubiquitousKeyValueStoreDidChangeExternally),
+                                               name: NSUbiquitousKeyValueStore.didChangeExternallyNotification,
+                                               object: iCloudKeyStore)
+    }
+    
+    @objc func ubiquitousKeyValueStoreDidChangeExternally() {
+        targetTime = iCloudKeyStore!.double(forKey: iCloudSavedTargetTimeKey)
     }
 }
--- Pomodoro/Controllers/TimerViewController.swift
+++ Pomodoro/Controllers/TimerViewController.swift
@@ -9,10 +9,13 @@
 import Foundation
 import DynamicColor
 import SwiftIconFont
+import ViewAnimator
 
 class TimerViewController: UIViewController {
     var scaleView: UIView?
     var widthPerSecond: CGFloat = 0
+    
+    var currentTime: Int = 0
     
     var indicatorLabel: UILabel = {
         let label = UILabel()
@@ -23,29 +26,54 @@ class TimerViewController: UIViewController {
         return label
     }()
     
+    var timerLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 30);
+        label.textAlignment = .center
+        label.textColor = .white
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.PresetColors.Red
+        view.backgroundColor = UIColor.PresetColors.Green
         
         view.addSubview(indicatorLabel)
+        view.addSubview(timerLabel)
     }
     
     override func viewDidLayoutSubviews() {
         widthPerSecond = self.view.frame.width / 9 / 60;
         
-        scaleView?.removeFromSuperview()
-        scaleView = ScaleView(frame: self.view.frame, gridScale: 1 / 9)
-        scaleView?.backgroundColor = .clear
-        view.addSubview(scaleView!)
+        if (scaleView == nil) {
+            scaleView?.removeFromSuperview()
+            scaleView = ScaleView(frame: self.view.frame, gridScale: 1 / 9)
+            scaleView?.backgroundColor = .clear
+            view.addSubview(scaleView!)
+        }
         
         indicatorLabel.frame = CGRect(x: 0,
                                       y: view.frame.height / 2,
                                       width: view.frame.width,
                                       height: 50.0)
+        
+        timerLabel.frame = CGRect(x: 0,
+                                  y: view.frame.height / 4 * 3,
+                                  width: view.frame.width,
+                                  height: 50.0)
+        
+        scaleView?.center = CGPoint(x: CGFloat(30 * 60 - currentTime) * widthPerSecond,
+                                    y: scaleView!.center.y)
+        
+        UIView.animate(withDuration: 0.5) {
+            self.view.backgroundColor = self.currentTime == 0 ? UIColor.PresetColors.Green : UIColor.PresetColors.Red
+        }
     }
     
     func setTime(second: Int) {
-        scaleView?.center = CGPoint(x: CGFloat(30 * 60 - second) * widthPerSecond, y: scaleView!.center.y)
+        currentTime = second
+        timerLabel.text = String(format: "%02d:%02d", currentTime / 60, currentTime % 60)
+        self.view.setNeedsLayout()
     }
 }
